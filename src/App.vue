e1c46e959bc905860e950f838c6cb09429f6a575
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2021-02-24 22:41:08 +0100

    feat(styles): add basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <input type="file" @change="handleFileChange" />
+  <h1>
+    FFMPEG in the interwebs
+  </h1>
+  <div class="links">
+    Thanks to <a href="https://github.com/jeromewu">@jeromewu</a> for <a href="https://github.com/ffmpegwasm/ffmpeg.wasm">ffmpeg.wasm</a>
+  </div>
+
+  <label for="fileSelector" class="button">Select .mov file</label>
+  <input id="fileSelector" type="file" @change="handleFileChange" class="file-input" />
 
   <div v-if="hasItems">
-    <div v-for="item in items">
-      <div>State:     {{ item.state }}</div>
-      <div>Video URL: {{ item.resultSource }}</div>
-
+    <div v-for="item in items" class="list-item">
       <div v-if="item.isConverting" class="progress-bar">
         <div class="progress-bar-wrapper">
           <div class="progress-bar-inner" :style="{ width: `${item.progress * 100}%` }"></div>
         </div>
+        <div class="progress-bar-state">{{ item.state }}</div>
         <div class="progress-bar-value">{{ (item.progress * 100).toFixed(2) }}%</div>
       </div>
 
       <div v-if="item.isCompleted">
-        <img :src="item.resultSource" />
+        <div class="converted-file-source">{{ item.resultSource }}</div>
+        <img :src="item.resultSource" class="converted-file-item" />
       </div>
     </div>
   </div>
@@ -40,24 +47,65 @@ const handleFileChange = async (event) => {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  width: min(400px, 100%);
+  margin: 60px auto;
+}
+
+h1 {
+  font-weight: normal;
+  margin: 0 0 0.3rem;
+}
+
+.links {
+  color: #666;
+  font-style: italic;
+  margin-bottom: 2rem;
+}
+
+.button {
+  background-color: #ff1072;
+  color: #fff;
+  padding: 0.6rem 1.2rem;
+  width: 100%;
+  display: block;
+  outline: 0;
+  border: 0;
+  transition: all 200ms ease-in-out;
+  text-align: center;
+  box-sizing: border-box;
+  margin-bottom: 4rem;
+}
+
+.button:active {
+  transform: rotate(3deg) scale(0.98);
+}
+
+.file-input {
+  display: none;
+}
+
+.links a[href] {
+  color: #ff1072;
 }
 
 .progress-bar {
   display: grid;
   grid-gap: 0.3rem;
+  grid-template-areas: 'bar bar' 'state value';
 }
 
 .progress-bar-wrapper {
-  background-color: #9b88ff;
+  grid-area: bar;
+  background-color: #ffa3c7;
   position: relative;
   height: 6px;
   border-radius: 6px;
+  overflow: hidden;
 }
 
 .progress-bar-inner {
@@ -65,7 +113,35 @@ const handleFileChange = async (event) => {
   top: 0;
   left: 0;
   height: 100%;
-  background-color: #3d18ff;
+  background-color: #ff1072;
   transition: all 300ms ease-in-out;
+}
+
+.progress-bar-state {
+  grid-area: state;
+  align-self: start;
+}
+
+.progress-bar-value {
+  grid-area: value;
+  text-align: end;
+  font-weight: bold;
+}
+
+.list-item {
+  margin-bottom: 2rem;
+  padding-bottom: 2rem;
+  border-bottom: 1px solid #ff1072;
+}
+
+
+.converted-file-source {
+  margin-bottom: 1rem;
+}
+
+.converted-file-item {
+  display: block;
+  width: 100%;
+  height: auto;
 }
 </style>
